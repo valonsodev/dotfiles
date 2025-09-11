30c0a3b0d361461ae52fff4e5f50e60e456771f6
valonso <80985309+valonsodev@users.noreply.github.com>
2025-09-12 00:14:45 +0200

    00:14 12/09/2025  -  13bec3482bff945f660c7d6a

--- home/dot_config/rofi/launcher/executable_launcher.sh
+++ home/dot_config/rofi/launcher/executable_launcher.sh
@@ -2,5 +2,5 @@
 
 rofi -no-lazy-grab -show drun -terminal foot \
 -modi filebrowser,drun,window \
--run-shell-command '{terminal} -e zsh -ic "{cmd} && read"' \
+-run-shell-command '{terminal} -e fish -ic "{cmd} && read"' \
 -theme ~/.config/rofi/launcher/launcher.rasi
